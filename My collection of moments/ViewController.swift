f7dedb84d58246d9fb2153b15c81166edddd0b48
eduardoquintero117 <eduardoquiju@hotmail.com>
2021-04-10 22:11:37 -0500

    Initial Commit

---
+++ My collection of moments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My collection of moments
+//
+//  Created by Eduardo Quintero on 10/04/21.
+//  Copyright © 2021 new x. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
